56dd85784a4874729016559e9ba5696e31afe57d
NinoNonikashvili <nonikashvilinino8799@gmail.com>
2023-01-20 18:47:42 +0400

    functions added to process search

--- Controller.php
+++ Controller.php
@@ -10,6 +10,8 @@
  */
 
  namespace User\BeerWebsite;
+ use User\BeerWebsite\model\Data;
+ use User\BeerWebsite\Router;
 
 /**
  * Controller
@@ -24,21 +26,63 @@
 class Controller
 {
     /**
-     * Get/post requests on home page are handled by this function
+     * Get requests on home page are handled by this function
+     * 
+     * @param Router $router router instance to access data instance
      * 
      * @return void
      */
-    public static function index()
+    public static function index(Router $router)
     {
-        echo 'index page is visited';
+        echo 'index page is visited with get request';
+        //$router->render();
+    }
+
+    /**
+     * Get requests on home page in two cases:
+     * 1. user entered keyword -> multiple appropriate data is returne
+     *    and rendered in dropdown below search input. ?keyword from url
+     * 2. user clicked desired data -> corresponding data details are
+     *    rendered on the page. ?clicked from url
+     * 
+     * @param Router $router router instance to access data instance
+     * 
+     * @return void
+     */
+    public static function search(Router $router)
+    {
+        echo 'search process started';
+        $keyword = $_GET['keyword'] ?? null;
+        $clicked = $_GET['clicked'] ?? null;
+        if ($keyword) {
+            $data = $router->db->fetchData('beer_name='.$keyword);
+            //$router->render(); //display names of beers in dropdwon or error
+        } else if ($clicked) {
+            $data = $router->db->fetchData('beer_name='.$clicked);
+            //format as beer object
+            $beer = $router->db->jsonToBeerObj($data[0]);
+            //save on db
+            $router->db->saveOnDB($beer);
+            //get from db using name stored in $clicked
+            $beerToRender = $router->db->getFromDB($clicked);
+            //pass to render
+            echo '<pre>';
+            var_dump($beerToRender); 
+            echo '</pre>';
+
+            //$router->render();
+        }
+      
     }
 
     /**
      * Get/post requests on storedData page are handled by this function.
      * 
+     * @param Router $router router instance to access data instance
+     * 
      * @return void
      */
-    public static function storedData()
+    public static function storedData(Router $router)
     {
         echo 'stored data page visited';
     }
